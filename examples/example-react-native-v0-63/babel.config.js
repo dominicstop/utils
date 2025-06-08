19ea6c1088ca12ac58b6e2fba87e708d7098a499
Dominic <dominic@dominicgo.dev>
2025-06-09 07:31:43 +0800

    ⚙️ Chore: Ex - Setup Imports for Library in `example-react-native-v0-63`

--- examples/example-react-native-v0-63/babel.config.js
+++ examples/example-react-native-v0-63/babel.config.js
@@ -1,3 +1,17 @@
+const path = require('path');
+const utilsPackage = require('../../package.json');
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+        alias: {
+          [utilsPackage.name]: path.join(__dirname, '../..', utilsPackage.source),
+        },
+      },
+    ],
+  ],
 };
