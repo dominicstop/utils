1a658f1d1e6990c01c82f47fadc37b8965ff601a
Dominic <dominic@dominicgo.dev>
2025-06-08 08:45:03 +0800

    🐞 Fix: Add Exports for `src/physics`

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './geometry';
 export * from './helpers';
+export * from './physics';
 
 export type * from './types/UtilityTypes';
 export type * from './types/Cloneable';
---
+++ src/physics/index.ts
@@ -0,0 +1,16 @@
+
+export * from './Particle';
+export * from './PhysicsEngine';
+
+export * from './CentralAttractionForce';
+export * from './DampingForce';
+export * from './RepulsionForce';
+
+export type * from './AnyParticle';
+export type * from './SomeShapeParticle';
+export type * from './SomeParticleForce';
+export type * from './SomeSystemForce';
+
+export type * from './SomePhysicsEngine';
+export type * from './SomeOfflineSimulation';
+export type * from './SomeRealtimeSimulation';
