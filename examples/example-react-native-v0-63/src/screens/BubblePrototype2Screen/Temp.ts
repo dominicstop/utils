1c2eba8853ba0b867d6f61807fe809134a07a068
Dominic <dominic@dominicgo.dev>
2025-06-12 07:59:40 +0800

    ⭐️ Impl: Ex - `useDeferredExecution`

--- examples/example-react-native-v0-63/src/screens/BubblePrototype2Screen/Temp.ts
+++ examples/example-react-native-v0-63/src/screens/BubblePrototype2Screen/Temp.ts
@@ -8,7 +8,7 @@ export type LoadingStateWithData<T, U> = {
     mode: 'LOADING';
 } | {
     mode: 'LOADED';
-    loadedData: T; 
+    loadedData: T;
 } | {
     mode: 'LOADING_ERROR';
     errorData: U;
@@ -35,7 +35,7 @@ export function useAsyncLazyRef<T>(
     onReject: ((reason: unknown) => void) | undefined = undefined,
     completion: ((resolvedValue: T) => void) | undefined = undefined
 ): [
-    ref: MutableRefObject<T | undefined>, 
+    ref: MutableRefObject<T | undefined>,
     reset: () => void,
 ] {
 
@@ -60,3 +60,66 @@ export function useAsyncLazyRef<T>(
         },
     ];
 };
+
+
+
+type DeferredExecutionState<U> = {
+  mode: 'TO_BE_INVOKED'
+} | {
+  mode: 'INVOKED';
+  result: U
+};
+
+export function useDeferredExecution<T, U>(
+  closureProvider: () => (args: T) => U,
+  onInvoke?: (result: U) => void
+): {
+  invoke: (args: T) => void;
+  reset: () => void;
+  result: U | undefined;
+  didInvoked: boolean;
+} {
+  const [state, setState] = React.useState<DeferredExecutionState<U>>({
+    mode: 'TO_BE_INVOKED',
+  });
+
+  const closureRef = React.useRef<(() => (args: T) => U) | null>(null);
+  if (!closureRef.current) {
+    closureRef.current = closureProvider;
+  }
+
+  const invoke = React.useCallback((args: T) => {
+    if (state.mode === 'INVOKED') return;
+
+    const result = closureRef.current!()(args);
+    setState({ mode: 'INVOKED', result });
+    onInvoke?.(result);
+  }, [state.mode, onInvoke]);
+
+  const reset = React.useCallback(() => {
+    setState({ mode: 'TO_BE_INVOKED' });
+  }, []);
+
+  return {
+    invoke,
+    reset,
+    result: state.mode === 'INVOKED' ? state.result : undefined,
+    didInvoked: state.mode === 'INVOKED',
+  };
+};
+
+type AsyncInvocationState<U> = {
+  mode: 'PENDING_INVOCATION';
+
+} | {
+  mode: 'INVOCATING';
+
+} | {
+  mode: 'INVOCATED';
+  result: U
+
+} | {
+  mode: 'INVOCATION_ERROR';
+  error: string
+};
+
